b5be47219cb279ad9eb21ce29d86d9dcc9ad4f60
SudoWaster <cezaryre@me.com>
2016-01-21 23:29:29 +0100

    Added working encoding

--- huffmancoding/Coders/HuffmanEncoder.java
+++ huffmancoding/Coders/HuffmanEncoder.java
@@ -18,26 +18,120 @@
 
 package huffmancoding.Coders;
 
+import huffmancoding.TextTools.HuffmanCharacter;
 import huffmancoding.TextTools.OccurrenceCounter;
 import huffmancoding.Tree.HuffmanTree;
+import java.util.ArrayList;
 
 /**
+ * An encoder class that uses Huffman coding
  *
  * @author cezary
  */
 public class HuffmanEncoder {
 
+    /**
+     * Init class with text - count characters and make a tree
+     *
+     * @param text a String with input
+     */
     public HuffmanEncoder(String text) {
         this.text = text;
         this.counter = new OccurrenceCounter(text);
         this.tree = new HuffmanTree(counter.getFullOccurrence());
     }
 
+    /**
+     * Encode the given String to a byte array
+     *
+     * @return an encoded Byte array
+     */
+    public Byte[] getEncoded() {
+        //
+        // Make sure we have ids and init our bytelist
+        //
+        HuffmanTree.updateIDs(tree.root, "");
+
+        ArrayList<Byte> bytelist = new ArrayList<Byte>();
+
+        //
+        // Init our leftover bits buffer
+        //
+        String leftover = new String();
+
+        //
+        // Go through the string
+        //
+        for(int i = 0; i < text.length(); i++) {
+            //
+            // Make a collective buffer and init it with leftover bits
+            //
+            String collectiveBuffer = leftover;
+
+            //
+            // Get current character corresponding object and id
+            //
+            HuffmanCharacter currentCharacter = tree.get(text.charAt(i));
+
+            collectiveBuffer += currentCharacter.getID();
+
+            //
+            // Make sure we do not extend the byte
+            //
+            while(collectiveBuffer.length() >= 8) {
+                //
+                // Cut first byte and convert it
+                //
+                String tempByteBuffer = collectiveBuffer.substring(0, 8);
+                collectiveBuffer = collectiveBuffer.substring(8);
+
+                byte tempByte = (byte) Integer.parseInt(tempByteBuffer, 2);
+
+                //
+                // Add to the bytelist
+                //
+                bytelist.add(tempByte);
+            }
+
+            //
+            // Save the leftovers for the next character
+            //
+            leftover = collectiveBuffer;
+        }
+
+        //
+        // Don't forget the leftovers
+        //
+        if(leftover.length() > 0) {
+            bytelist.add(Byte.parseByte(leftover, 2));
+        }
+
+        //
+        // Return the resulting array
+        //
+        return bytelist.toArray(new Byte[bytelist.size()]);
+    }
 
 
-    public HuffmanTree tree;
+    public Byte[] getDictionary() {
+        //
+        // TODO: Make a method returning a byte array for dictionary headers.
+        //      Should use a byte for the id length and a following space
+        //      for the id. It should utilize at least 256 bytes for every
+        //      ASCII character (length, id, length, id, length, id etc.)
+        //
+        return null;
+    }
+
+    //
+    // Our text related objects
+    //
     public OccurrenceCounter counter;
+    public HuffmanTree tree;
 
+    //
+    // The input string
+    //
     protected String text;
     
 }
--- huffmancoding/Main.java
+++ huffmancoding/Main.java
@@ -34,24 +34,46 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        OccurrenceCounter c = new OccurrenceCounter("Ala ma kota, a kot ma AIDS");
+
+        String input = "Copyright C Cezary Regec SudoWaster " +
+            "This program is free software you can redistribute it andor modify it under the terms of the GNU General Public License as published by the Free Software Foundation either version of the License or at your option any later version " +
+            "This program is distributed in the hope that it will be useful but WITHOUT ANY WARRANTY without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE  See the GNU General Public License for more details " +
+            "You should have received a copy of the GNU General Public License along with this program if not write to the Free Software Foundation Inc Temple Place Suite Boston MA USA";
+
+        //
+        // Test occurrence count, sorting and tree creation
+        //
+        OccurrenceCounter c = new OccurrenceCounter(input);
         CharacterOccurrence[] o = c.getFullOccurrence();
         Occurrence.sort(o);
         HuffmanTree tree = new HuffmanTree();
         
         for(CharacterOccurrence e : o) {
             tree.insert(e);
-            System.out.println(e.getCharacter() + " - " + e.getOccurrence());
         }
 
-        tree.updateIDs(tree.root, "0");
-        tree.hasNullNodes(tree.root);
+        HuffmanTree.updateIDs(tree.root, "");
 
-        System.out.println(tree.get('S').getOccurrence() + " " + tree.get('S').getID());
-        System.out.println(tree.get(' ').getOccurrence() + " " + tree.get(' ').getID());
-        System.out.println(tree.get("0000001").getCharacter());
+        //
+        // Tree verification
+        //
+        for(CharacterOccurrence e : o) {
+            System.out.println(e.getCharacter() + " - " + e.getOccurrence() + " - " + tree.get(e.getCharacter()).getID());
+        }
 
-        HuffmanEncoder e = new HuffmanEncoder("ala ma kota, a kot ma aids");
+        //
+        // Test encoding
+        //
+        HuffmanEncoder he = new HuffmanEncoder(input);
+        Byte[] encoded = he.getEncoded();
+
+        String result = new String();
+        for(Byte e : encoded) {
+            result += (char)e.byteValue();
+        }
+
+        System.out.println(input + "\n" + result);
+        System.out.println((double)result.length()/input.length());
     }
 
 }
